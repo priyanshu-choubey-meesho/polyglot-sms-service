6bf6579f0701e46411385a9685fdc6709185cd4a
Priyanshu <f20220318@goa.bits-pilani.ac.in>
2025-12-28 16:50:13 +0530

    Implemented error handling in the

--- sms-sender/src/main/java/com/example/demo/service/SmsService.java
+++ sms-sender/src/main/java/com/example/demo/service/SmsService.java
@@ -1,29 +1,43 @@
 package com.example.demo.service;
+
 import org.springframework.stereotype.Service;
 import com.example.demo.service.BlacklistCache;
 import com.example.demo.model.SmsRequest;
 import com.example.demo.model.SmsEvent;
+
 @Service
 public class SmsService {
     private final BlacklistCache cache;
     private final SmsEventProducer eventProducer;
     private final TwillioService twillioService;
+
     public SmsService(BlacklistCache cache, SmsEventProducer eventProducer, TwillioService twillioService) {
         this.cache = cache;
         this.eventProducer = eventProducer;
         this.twillioService = twillioService;
-    }    
+    }
+
     public String sendSms(SmsRequest request) {
         String phoneNumber = request.getPhoneNumber();
         String message = request.getMessage();
-        if(cache.isBlacklisted(phoneNumber)) {
-            SmsEvent event = new SmsEvent(phoneNumber, message, "Failed: Phone number is blacklisted");
+
+        // Check if phone number is blacklisted
+        if (cache.isBlacklisted(phoneNumber)) {
+            SmsEvent event = new SmsEvent(phoneNumber, message, "blocked");
             eventProducer.sendSmsEvent(event);
             return "Failed: Phone number is blacklisted";
         }
-        twillioService.sendSms(phoneNumber, message);
-        SmsEvent event = new SmsEvent(phoneNumber, message, "Sent");
-        eventProducer.sendSmsEvent(event);
-        return "SMS sent to " + request.getPhoneNumber();
+
+        // Try to send SMS
+        try {
+            twillioService.sendSms(phoneNumber, message);
+            SmsEvent event = new SmsEvent(phoneNumber, message, "successful");
+            eventProducer.sendSmsEvent(event);
+            return "SMS sent to " + request.getPhoneNumber();
+        } catch (Exception e) {
+            SmsEvent event = new SmsEvent(phoneNumber, message, "unsuccessful");
+            eventProducer.sendSmsEvent(event);
+            return "Failed to send SMS: " + e.getMessage();
+        }
     }
 }
