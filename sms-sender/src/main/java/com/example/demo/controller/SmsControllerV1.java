c70fc93ae033932176388a7088ee015703881268
Priyanshu Choubey <priyanshu.choubey@meesho.com>
2026-01-27 18:36:13 +0530

    Fixed 'result' variable not found error in Controller

--- sms-sender/src/main/java/com/example/demo/controller/SmsControllerV1.java
+++ sms-sender/src/main/java/com/example/demo/controller/SmsControllerV1.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
 import com.example.demo.model.SmsResponse;
 import com.example.demo.service.SmsService;
 import com.example.demo.model.SmsRequest;
@@ -24,8 +24,9 @@ public class SmsControllerV1 {
 
     @PostMapping
     public ResponseEntity<SmsResponse> sendSmsRequest(@Valid @RequestBody SmsRequest request) {
+        String result = null;
         try {
-            String result = service.sendSms(request);
+            result = service.sendSms(request);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new SmsResponse("Server error kindly try again later"));
         }
